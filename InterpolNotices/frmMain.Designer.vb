e8ae472bccb156be2a83053ade3dc30767e99fea
Christian Schmidt <47923480+chrisschm@users.noreply.github.com>
2023-01-06 20:54:42 +0100

    Init

---
+++ InterpolNotices/frmMain.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.ListView = New System.Windows.Forms.ListView()
+        Me.entity_id = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.forename = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.lastname = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.date_of_birth = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.nationalities = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.StatusStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(594, 22)
+        Me.StatusStrip.TabIndex = 0
+        '
+        'ToolStripStatusLabel
+        '
+        Me.ToolStripStatusLabel.Name = "ToolStripStatusLabel"
+        Me.ToolStripStatusLabel.Size = New System.Drawing.Size(21, 17)
+        Me.ToolStripStatusLabel.Text = "##"
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 1000
+        '
+        'ListView
+        '
+        Me.ListView.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.entity_id, Me.forename, Me.lastname, Me.date_of_birth, Me.nationalities})
+        Me.ListView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView.FullRowSelect = True
+        Me.ListView.HideSelection = False
+        Me.ListView.Location = New System.Drawing.Point(0, 0)
+        Me.ListView.MultiSelect = False
+        Me.ListView.Name = "ListView"
+        Me.ListView.Size = New System.Drawing.Size(594, 428)
+        Me.ListView.TabIndex = 1
+        Me.ListView.UseCompatibleStateImageBehavior = False
+        Me.ListView.View = System.Windows.Forms.View.Details
+        '
+        'entity_id
+        '
+        Me.entity_id.Text = "ID"
+        Me.entity_id.Width = 90
+        '
+        'forename
+        '
+        Me.forename.Text = "Vorname"
+        Me.forename.Width = 120
+        '
+        'lastname
+        '
+        Me.lastname.Text = "Nachname"
+        Me.lastname.Width = 150
+        '
+        'date_of_birth
+        '
+        Me.date_of_birth.Text = "Gebutsdatum"
+        Me.date_of_birth.Width = 100
+        '
+        'nationalities
+        '
+        Me.nationalities.Text = "Nationalitäten"
+        Me.nationalities.Width = 100
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(594, 450)
+        Me.Controls.Add(Me.ListView)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "MainForm"
+        Me.Text = "Interpol Notices"
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents ToolStripStatusLabel As ToolStripStatusLabel
+    Friend WithEvents Timer As Timer
+    Friend WithEvents ListView As ListView
+    Friend WithEvents entity_id As ColumnHeader
+    Friend WithEvents forename As ColumnHeader
+    Friend WithEvents lastname As ColumnHeader
+    Friend WithEvents date_of_birth As ColumnHeader
+    Friend WithEvents nationalities As ColumnHeader
+End Class
---
+++ InterpolNotices/frmMain.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Net
+Imports Newtonsoft.Json.Linq
+
+Public Class MainForm
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        ToolStripStatusLabel.Text = ""
+        Timer.Enabled = True
+
+    End Sub
+
+
+
+
+
+
+
+    Private Sub GetNotices()
+
+        ToolStripStatusLabel.Text = "Erstelle Interpol Anfrage..."
+        StatusStrip.Refresh()
+        Me.Refresh()
+        Application.DoEvents()
+
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+        Dim jsonfull As String = New System.Net.WebClient().DownloadString("https://ws-public.interpol.int/notices/v1/red?&resultPerPage=200")
+        Dim parsejson As JObject = JObject.Parse(jsonfull)
+        For Each Notice In parsejson.SelectToken("_embedded.notices")
+            Dim LVItem As ListViewItem = ListView.Items.Add(Notice.SelectToken("entity_id").ToString())
+            LVItem.SubItems.Add(Notice.SelectToken("forename").ToString())
+            LVItem.SubItems.Add(Notice.SelectToken("name").ToString())
+            LVItem.SubItems.Add(CDate(Notice.SelectToken("date_of_birth").ToString()).ToShortDateString)
+            LVItem.SubItems.Add(cleanString(Notice.SelectToken("nationalities").ToString()))
+        Next
+
+        ToolStripStatusLabel.Text = ""
+
+    End Sub
+
+    Private Function cleanString(ByVal s As String) As String
+
+        Dim tmp As String = s.Replace("[", "")
+        tmp = tmp.Replace("]", "")
+        tmp = tmp.Replace("""", "")
+        Return tmp
+
+    End Function
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+
+        Timer.Enabled = False
+        GetNotices()
+
+    End Sub
+
+    Private Sub ListView_ColumnClick(sender As Object, e As ColumnClickEventArgs) Handles ListView.ColumnClick
+
+        Dim Sort_Order As Integer = SortOrder.Ascending
+        If ListView.Columns.Item(e.Column).ListView.Sorting <> SortOrder.Ascending Then
+            Sort_Order = SortOrder.Ascending
+        Else
+            Sort_Order = SortOrder.Descending
+        End If
+        ListView.Columns.Item(e.Column).ListView.Sorting = Sort_Order
+
+        If e.Column = 3 Then
+            Dim LVSorter = New ListViewItemDateComparer(e.Column, Sort_Order)
+            ListView.ListViewItemSorter = LVSorter
+        Else
+            Dim LVSorter = New ListViewItemComparer(e.Column, Sort_Order)
+            ListView.ListViewItemSorter = LVSorter
+        End If
+
+    End Sub
+End Class
+
+Friend Class ListViewItemComparer
+    Implements IComparer
+    Private col As Integer
+    Private _sort As SortOrder = SortOrder.Ascending
+
+    Public Sub New(column As Integer, sort As Windows.Forms.SortOrder)
+        col = column
+        _sort = sort
+    End Sub
+
+    Public Function Compare(x As Object, y As Object) As Integer Implements System.Collections.IComparer.Compare
+
+        Dim returnVal As Integer = -1
+        If (_sort = 1) Then
+            returnVal = [String].Compare(CType(x, ListViewItem).SubItems(col).Text, CType(y, ListViewItem).SubItems(col).Text)
+        Else
+            returnVal = [String].Compare(CType(y, ListViewItem).SubItems(col).Text, CType(x, ListViewItem).SubItems(col).Text)
+        End If
+
+        Return returnVal
+
+    End Function
+
+End Class
+
+Friend Class ListViewItemDateComparer
+
+    Implements IComparer
+    Private col As Integer
+    Private _sort As SortOrder = SortOrder.Ascending
+
+    Public Sub New(column As Integer, sort As Windows.Forms.SortOrder)
+        col = column
+        _sort = sort
+    End Sub
+
+    Public Function Compare(x As Object, y As Object) As Integer Implements System.Collections.IComparer.Compare
+        Dim returnVal As Integer = -1
+
+        ' parse LV contents back to DateTime value
+        Dim dtX As DateTime = DateTime.Parse(CType(x, ListViewItem).SubItems(col).Text)
+        Dim dtY As DateTime = DateTime.Parse(CType(y, ListViewItem).SubItems(col).Text)
+
+        ' compare
+        returnVal = DateTime.Compare(dtX, dtY)
+
+        If _sort = SortOrder.Descending Then
+            returnVal *= -1
+        End If
+        Return returnVal
+
+    End Function
+End Class
